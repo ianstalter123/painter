ba4e2a0c813b5fed0459ec296ce4669f03519467
ianstalter123 <istalter66@gmail.com>
2015-05-14 20:44:22 -0700

    added a gradeint with help

--- script.js
+++ script.js
@@ -4,47 +4,45 @@ console.log("JavaScript is alive");
 var currentColor = 'blue';
 
 var paint= function() {
-var grid = document.querySelectorAll("div");
-for(var i = 0; i < grid.length; i++)
-{
-grid[i].addEventListener("mousedown", function(){
-this.style.background = currentColor;
-
-for(var i = 0; i < grid.length; i++)
-{
-grid[i].className = grid[i].className + "drag";
-
-}
-})
-
-grid[i].addEventListener("mouseover", function(event) 
-	{
-		if(this.className === "drag")
+		var grid = document.querySelectorAll("div");
+		for(var i = 0; i < grid.length; i++)
 		{
-		this.style.background = currentColor;}}, false);
+			grid[i].addEventListener("mousedown", function(){
+			this.style.background = currentColor;
 
-grid[i].addEventListener("mouseup", function(){
-this.style.background = currentColor;
+		for(var i = 0; i < grid.length; i++)
+		{
+			grid[i].className = grid[i].className + "drag";
 
-for(var i = 0; i < grid.length; i++)
-{
-grid[i].className = "";
-}
-})
+		}
+		})
 
+		grid[i].addEventListener("mouseover", function(event) 
+			{
+				if(this.className === "drag")
+				{
+				this.style.background = currentColor;}}, false);
 
-}}
+		grid[i].addEventListener("mouseup", function(){
+		this.style.background = currentColor;
+
+		for(var i = 0; i < grid.length; i++)
+		{
+			grid[i].className = "";
+		}
+		})
+		}}
 
 var makePalette = function(color) {
-var square = document.createElement("div");
-		square.style.width = "3%";
-		square.style.height = "50px";
+	var square = document.createElement("div");
+		square.style.width = "1%";
+		square.style.height = "20px";
 		square.style.border = "3px solid grey";
 		square.style.background = color;
 		square.style.float = "left";
 		square.addEventListener("click", function(){
- currentColor = color;
- console.log("hello");
+ 	currentColor = color;
+ 	
 })
 		document.body.appendChild(square);
 }
@@ -67,13 +65,24 @@ for (var i =0 ; i<150; i++)
 }
 makeGrid();
 paint();
-makePalette('red');
-makePalette('blue');
-makePalette('green');
-makePalette('yellow');
-makePalette('pink');
-makePalette('brown');
-makePalette('black');
+var x =10;
+function RGB2Color(r,g,b)
+  {
+    return 'rgb(' + Math.round(r) + ',' + Math.round(g) + ',' + Math.round(b) + ')';
+  }
+var frequency = .3;
+for (var i = 0; i < 32; ++i)
+{
+	
+   red   = Math.sin(frequency*i + 0) * 127 + 128;
+   green = Math.sin(frequency*i + 2) * 127 + 128;
+   blue  = Math.sin(frequency*i + 4) * 127 + 128;
+
+   makePalette( '' + RGB2Color(red,green,blue) + '');
+}
+
+
+;
 }
 
 document.addEventListener("DOMContentLoaded", loadingFunction);
